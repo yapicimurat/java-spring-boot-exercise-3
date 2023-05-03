c48ab4155f5c9a394016f8111179818deb757269
yapicimurat <muratyapiicii@gmail.com>
2023-05-03 19:14:17 +0300

    Create delete controller

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -5,10 +5,10 @@ import org.example.dto.UserDto;
 import org.example.request.UserPostRequest;
 import org.example.request.UserPutRequest;
 import org.example.response.DataResponse;
+import org.example.response.Response;
 import org.example.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
@@ -44,10 +44,16 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<DataResponse<String>> update(@PathVariable("id") String id,@Valid @RequestBody UserPutRequest request) {
+    public ResponseEntity<DataResponse<String>> update(@PathVariable("id") String id, @Valid @RequestBody UserPutRequest request) {
         final DataResponse<String> userId = userService.update(id, request);
 
         return ResponseEntity.ok(userId);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> deleteById(@PathVariable("id") String id) {
+        final Response response = userService.delete(id);
+
+        return ResponseEntity.ok(response);
+    }
 }
